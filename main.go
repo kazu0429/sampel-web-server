72abcce70183516777ddd1a8a9552579749ad558
kazu0429 <116004457+Akimoto-IT0412@users.noreply.github.com>
2023-05-22 23:41:13 +0900

    Update main.go net/httpによる簡易Webサーバー構築

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+     "fmt"
+     "log"
+     "net/http" // HTTPを扱うパッケージ:クライアントとサーバーを実装する
+) 
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+     hello := []byte("Hello, World!!")
+     _, err := w.Write(hello)
+     if err != nil {
+          log.Fatal(err)
+     }
+}
 
 func main() {
-     fmt.Println("Hello")
+     http.HandleFunc("/hello", helloHandler)
+     fmt.Println("Server Start Up.......")
+     log.Fatal(http.ListenAndServe("localhost:8080",nil))
 }
