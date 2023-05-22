c98899fea2d30dc8d4ec8cfb8b1d80e882764de6
kazu0429 <116004457+Akimoto-IT0412@users.noreply.github.com>
2023-05-23 00:14:30 +0900

    html表示

--- main.go
+++ main.go
@@ -3,19 +3,23 @@ package main
 import (
      "fmt"
      "log"
+     "html/template"
      "net/http" // HTTPを扱うパッケージ:クライアントとサーバーを実装する
-) 
+)
 
-func helloHandler(w http.ResponseWriter, r *http.Request){
-     hello := []byte("Hello, World!!")
-     _, err := w.Write(hello)
+func viewHandler(w http.ResponseWriter, r *http.Request){
+     html, err := template.ParseFiles("go_playground.html")
      if err != nil {
+          log.Fatal(err)
+     }
+     if err := html.Execute(w, nil); err != nil{
           log.Fatal(err)
      }
 }
 
 func main() {
-     http.HandleFunc("/hello", helloHandler)
+     http.HandleFunc("/go", viewHandler)
+     fmt.Println("localhost:8080")
      fmt.Println("Server Start Up.......")
      log.Fatal(http.ListenAndServe("localhost:8080",nil))
 }
